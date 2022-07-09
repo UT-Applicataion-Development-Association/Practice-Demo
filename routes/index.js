b9f04642853876858236070df99824a25550fa95
Laura Lu <lulaura011205@gmail.com>
2022-07-09 16:35:00 -0700

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,34 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 /* Example */
-router.get('/', (req, res) => {
-  res.send('This is a GET API');
+router.get("/", (req, res) => {
+  res.send("This is a GET API");
 });
 
 /* TODO */
+router.post("/", (req, res) => {
+  res.send("This is a POST API");
+});
+
+router.put("/", (req, res) => {
+  res.send("This is a PUT API");
+});
+
+router.delete("/", (req, res) => {
+  res.send("This is a DELETE API");
+});
+
+router.post("/user/:id", (req, res) => {
+  if (req.params.id === "12345") {
+    res.send("User id is correct");
+  } else {
+    res.send("User id is wrong");
+  }
+});
+
+router.get("/addFive/:num", (req, res) => {
+  res.send(req.params.num + 5);
+});
 
 module.exports = router;
