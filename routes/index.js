00983dd94e48b930e2ad99aa3922f3a6ee659fc6
Sue-Tong <tong.su@mail.utoronto.ca>
2022-07-09 18:09:33 -0400

    step 1-5

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,31 @@ router.get('/', (req, res) => {
 });
 
 /* TODO */
-
 module.exports = router;
+
+//Task 1-5
+router.use('/', (req, res, next) => {
+  console.log('This is a GET API', req.get)
+  next()
+}, (req, res, next) => {
+  console.log('This is a POST API', req.post)
+  next()
+}, (req, res, next) => {
+  console.log('This is a PUT API', req.put)
+  next()
+}, (req, res, next) => {
+  console.log('This is a DELETE API', req.delete)
+})
+
+//Task 6
+// a middleware sub-stack that handles POST requests to the /user/:id path
+router.post('/user/:id', (req, res, next) => {
+  // if the user ID is 12345, skip to the next router
+  if (req.params.id === '12345') res.send('User id is correct');
+  // otherwise pass control to the next middleware function in this stack
+  else next()
+}, (req, res, next) => {
+  // render a regular page
+  res.send(' User id is wrong');
+})
+
