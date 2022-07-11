91a046ed88912c0568b8bfabf73afc2e217c969b
Laura Lu <lulaura011205@gmail.com>
2022-07-10 17:10:40 -0700

    updated router ordering

--- routes/index.js
+++ routes/index.js
@@ -1,43 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-// /* Example */
-// router.get("/", (req, res) => {
-//   res.send("This is a GET API");
-// });
-
-// /* TODO */
-
-router.use(
-  "/",
-  (req, res, next) => {
-    if (req.method === "GET") {
-      res.send("This is a GET API");
-    } else {
-      next();
-    }
-  },
-  (req, res, next) => {
-    if (req.method === "POST") {
-      res.send("This is a POST API");
-    } else {
-      next();
-    }
-  },
-  (req, res, next) => {
-    if (req.method === "PUT") {
-      res.send("This is a PUT API");
-    } else {
-      next();
-    }
-  },
-  (req, res) => {
-    if (req.method === "DELETE") {
-      res.send("This is a DELETE API");
-    }
-  }
-);
-
 router.post("/user/:id", (req, res) => {
   if (req.params.id === "12345") {
     res.send("User id is correct");
@@ -48,6 +11,10 @@ router.post("/user/:id", (req, res) => {
 
 router.get("/addFive/:num", (req, res) => {
   res.send(req.params.num + 5);
+});
+
+router.use("/", (req, res, next) => {
+  res.send("This is a " + req.method + " API");
 });
 
 module.exports = router;
