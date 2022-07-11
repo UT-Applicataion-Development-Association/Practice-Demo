feaaeaa160f72870bf3e64922aebf7ff390aff5c
Laura Lu <lulaura011205@gmail.com>
2022-07-10 17:20:04 -0700

    minor style fix

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,20 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-router.post("/user/:id", (req, res) => {
-  if (req.params.id === "12345") {
-    res.send("User id is correct");
+router.post('/user/:id', (req, res) => {
+  if (req.params.id === '12345') {
+    res.send('User id is correct');
   } else {
-    res.send("User id is wrong");
+    res.send('User id is wrong');
   }
 });
 
-router.get("/addFive/:num", (req, res) => {
+router.get('/addFive/:num', (req, res) => {
   res.send((parseInt(req.params.num) + 5).toString());
 });
 
-router.use("/", (req, res, next) => {
-  res.send("This is a " + req.method + " API");
+router.use('/', (req, res, next) => {
+  res.send('This is a ' + req.method + ' API');
 });
 
 module.exports = router;
