ebc51dbe205f0406f8c41b9f3fab52a2976823f4
Sarah <lemiex@outlook.com>
2022-07-12 15:44:38 -0400

    satisfied google style standards

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/user/:id', (req, res, next) => {
+router.post('/user/:id', (req, res) => {
   if (req.params.id === '12345') {
     res.send('User id is correct');
-    next()
-  }
-  else {
+  } else {
     res.send('User id is wrong');
-    next()
   }
-})
+});
 
 router.get('/addFive/:num', (req, res) => {
   res.send((req.params.num + 5).toString);
-})
+});
 
-router.use('/', (req, res, next) => {
+router.use('/', (req, res) => {
   res.send('This is a ' + req.method + ' API');
-})
+});
 
 module.exports = router;
