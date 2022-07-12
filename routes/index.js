f1bf278e0b0d06cdd1686a54454c2b406195d923
Sarah <lemiex@outlook.com>
2022-07-12 15:22:33 -0400

    attempt1

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,27 @@
-const express = require('express');
-const router = express.Router();
+var express = require('express');
+var router = express.Router();
 
-/* Example */
-router.get('/', (req, res) => {
-  res.send('This is a GET API');
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
-/* TODO */
+router.use('/', (res, req, next) => {
+  res.send('This is a ' + req.method + ' API')
+  next()
+})
+
+router.post('/user/:id', (res, req) => {
+  if (req.params.id === '12345') {
+    res.send('User id is correct');
+  }
+  else {
+    res.send('User id is wrong');
+  }
+})
+
+router.get('/addFive/:num', (res, req) => {
+  res.send((req.params.num + 5).toString)
+})
 
 module.exports = router;
